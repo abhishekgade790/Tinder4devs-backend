3a9baa4f257dcf509abb62ce85514d5454668eec
Abhishek Gade <abhishekgade790@gmail.com>
2025-08-05 23:12:48 +0530

    handle cors for deployed server

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tinder4devs.vercel.app"],
     credentials: true
 }))
 app.use(express.json());
