3c7cb1954c83fd584986e3eb1535193bce06d1f3
Abhishek Gade <abhishekgade790@gmail.com>
2025-08-21 21:23:25 +0530

    fix: correct method to retrieve Razorpay webhook signature

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -60,9 +60,8 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 });
 
 //webhook
-//webhook
 paymentRouter.post("/payment/webhook", async (req, res) => {
-    const signature = req.headers["x-razorpay-signature"];
+    const signature = req.get["x-razorpay-signature"];
     const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
 
     const durationMapping = { "1": 30, "3": 90, "6": 180 };
